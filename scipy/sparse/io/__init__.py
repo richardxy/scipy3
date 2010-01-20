65673fdf217bf3aca3b5922f8b6f617a0592d455
David Cournapeau <cournape@gmail.com>
2010-01-20 19:49:33 +0900

    DOC: mention HB support in scipy.sparse.io module doc.

--- scipy/sparse/io/__init__.py
+++ scipy/sparse/io/__init__.py
@@ -1,6 +1,18 @@
 """
-Set of utilities to read/write file format specifics to scipy.sparse.
+Set of utilities to read/write file format specific to sparse matrices.
+
+Supported formats:
+    - Harwell-Boeing: read and write capabilities
 """
+from numpy.testing \
+    import \
+        Tester
+
 from scipy.sparse.io.hb \
     import \
         HBFile, HBInfo, read_hb, write_hb, MalformedHeader
+import hb
+
+__all__ = hb.__all__
+
+test = Tester().test
